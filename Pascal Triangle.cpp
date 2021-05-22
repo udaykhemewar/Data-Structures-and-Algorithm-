a1f06f05482ecba807e2f843a0f7e145e3c11bfc
Uday <udaykhemewar@gmai.com>
2021-05-23 00:33:05 +0530

    Final

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+void pascal (int n)
+{
+	int arr [n][n];
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<=i; j++)
+		{
+			if(j==0 || j==i)
+				arr[i][j]=1;
+			else
+				arr[i][j]=arr[i-1][j-1]+arr[i-1][j];
+		}
+	}
+	for (int i=0; i<n; i++)
+	{
+		for (int j=0; j<=i; j++)
+		{
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+
+}
+
+void pascalBetter (int n)
+{
+	for (int i=1; i<=n; i++)
+	{
+		int c=1;
+		for (int j=1; j<=i; j++)
+		{
+			cout<<c<<" ";
+			c = c * (i-j)/j;
+
+		}
+		cout<<endl;
+	}
+}
+int main()
+{
+	pascal(5);
+	pascalBetter(5);
+	return 0;
+}
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    cin>>a;
+   if (a=10)
+   {
+    cout<<"Hello Guys"<<endl;
+   }
+    return 0;
+}
